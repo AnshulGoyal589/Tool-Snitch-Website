c21ebc37a5fed880d5c6cff1e2185c3cddde4366
Dheeraj Sharma <greatnerve@gmail.com>
2024-09-25 23:51:38 +0530

    Theortical Dynamic Data Added

--- src/app/(device-form)/shops/[slug]/page.tsx
+++ src/app/(device-form)/shops/[slug]/page.tsx
@@ -1,9 +1,25 @@
 import { ReviewSection, ShopHero } from "@/components";
+import { api } from "@/api/api";
 
-export default function ShopDetailsPage(){
+export default async function ShopDetailsPage({params} : {params: {slug: string}}) {
+
+    async function getShopDetails(slug: string) {
+        try {
+            const response = await api.get(`/read/shopData/${slug}`);
+            return response.data;
+        } catch (error) {
+            // console.error(error.message);
+            return null;
+        }
+    }
+
+    const shop = await getShopDetails(params.slug);
+    console.log(shop);
+
     return (
+        
         <>
-        <ShopHero />
+        <ShopHero {...shop}/>
         <ReviewSection/>
         </>
     )
--- src/components/private/Shops/ShopHero.tsx
+++ src/components/private/Shops/ShopHero.tsx
@@ -3,23 +3,51 @@ import { Badge } from "@/components/ui/badge";
 import HeroCarousel from "../Carousel/HeroCarousel";
 import { Button } from "@nextui-org/react";
 
-export default function ShopHero() {
+interface ShopHeroProps {
+  shopName: string;
+  rating: number;
+  address: string;
+  timings: {
+    open: string;
+    close: string;
+  };
+  services: string[];
+  description: string;
+  images: { id: number; name?: string; url: string }[];
+  city: string;
+}
+
+export default function ShopHero({
+  shopName,
+  rating,
+  address,
+  timings,
+  services,
+  description,
+  images,
+  city,
+}: ShopHeroProps) {
   return (
     <div className="mx-3 mt-4 md:mx-8 lg:mx-16 xl:mx-20">
-      <h1 className="text-5xl font-bold">QuickFix Tech Solutions</h1>
+      <h1 className="text-5xl font-bold">{"QuickFix Tech Solutions"}</h1>
       <div className="mt-4 flex items-center">
         <div className="flex items-center gap-2">
           <span className="flex items-center gap-1 rounded-full bg-[#FFD700]/20 px-2 py-1">
-            <FaStar className="size-4 text-[#FFD700]" />
-            <FaStar className="size-4 text-[#FFD700]" />
-            <FaStar className="size-4 text-[#FFD700]" />
-            <FaStar className="size-4 text-[#FFD700]" />
-            <FaStar className="size-4 text-[#FFD700]" />
+            {Array.from({ length: 5 }, (_, index) => {
+              return (
+                <FaStar
+                  key={index}
+                  className={`${
+                    index < rating ? "text-[#FFD700]" : "text-gray-400"
+                  }`}
+                />
+              );
+            })}
           </span>
-          <span className="ml-2 text-sm text-gray-800">4.5</span>
+          <span className="ml-2 text-sm text-gray-800">{rating || 0 }</span>
         </div>
         <div className="ml-4 flex items-center gap-2">
-          <Badge variant="outline">Delhi</Badge>
+          <Badge variant="outline">{city}</Badge>
           <Badge variant="outline">10 Year+</Badge>
         </div>
       </div>
@@ -27,7 +55,7 @@ export default function ShopHero() {
         <div className="">
           <HeroCarousel
             name="product"
-            list={dummyData}
+            list={images || dummyData}
             orientation="horizontal"
           />
         </div>
@@ -35,6 +63,8 @@ export default function ShopHero() {
           <div>
             <h3 className="text-xl font-semibold">Description</h3>
             <p className="mt-2 text-gray-800">
+              {description ||
+              `
               At QuickFix Tech Solutions, we pride ourselves on offering fast,
               reliable repair services for smartphones, laptops, and tablets.
               With over 10 years of experience, our certified technicians ensure
@@ -44,24 +74,26 @@ export default function ShopHero() {
               prices. Visit us in Mumbai for a seamless repair experience, or
               take advantage of our doorstep service for ultimate convenience.m
               quos quas quae voluptatem nesciunt doloremque voluptatum.
+              `
+              }
             </p>
           </div>
-          <div className="mt-4 flex items-center gap-2">
+          <div className="mt-4 flex gap-2">
             <span className="text-lg font-semibold">Address: </span>
             <address className="text-gray-800">
-              A- 577, Street No. -12, Tyagi Rd, Bank Colony, Area 56, Mandoli
-              Extension, Delhi, 110093
+             {address || "Shop No. 1, Ground Floor, Shree Sai Darshan CHS, Opp. Datta Mandir, Near Datta Mandir, Datta Mandir Rd, Datta Mandir, Malad East, Mumbai, Maharashtra 400097"}
             </address>
           </div>
           <p className="mt-4">
             <span className="text-lg font-semibold">Timings: </span>
-            <span className="text-gray-800">10:00 AM - 8:00 PM</span>
+            <span className="text-gray-800">
+              {timings?.open || "10:00 AM"} - {timings?.close || "8:00 PM"}
+            </span>
           </p>
           <div className="mt-4">
             <span className="text-lg font-semibold">Services: </span>
             <span className="text-gray-800">
-              Smartphone Repair, Tempered Glass, Laptop Repair, Laptop
-              Servicing, RAM Upgrade, Storage Upgrade, Screen Replacement
+              {services?.join(", ") || "Screen Replacement, Battery Change, Software Update"}
             </span>
           </div>
           <div className="mt-4 flex items-center gap-2">
@@ -71,7 +103,11 @@ export default function ShopHero() {
             >
               Get Appinment{" "}
             </Button>
-            <Button radius="full" variant="bordered" className="text-lg border-[#D8BA74] text-[#D8BA74] py-6 px-4">
+            <Button
+              radius="full"
+              variant="bordered"
+              className="border-[#D8BA74] px-4 py-6 text-lg text-[#D8BA74]"
+            >
               Look for Other Stores
             </Button>
           </div>
@@ -84,6 +120,6 @@ export default function ShopHero() {
 const dummyData = Array.from({ length: 38 }, (_, index) => ({
   id: index + 1,
   name: `product${index + 1}`,
-  image:
+  url:
     "https://upload.wikimedia.org/wikipedia/commons/8/80/Aspect_ratio_-_16x9.svg",
 }));
