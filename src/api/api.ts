5fd29fc9cf5179b4caee559844c8fc03a0780cf9
AnshulGoyal589 <anshulgoyal589.com>
2024-09-27 03:52:58 +0530

    v3

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:5000',
+  baseURL: 'https://tools-snitch.yellowbush-cadc3844.centralindia.azurecontainerapps.io/',
     headers: {
         'Content-Type': 'application/json',
     },
