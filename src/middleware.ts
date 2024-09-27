656f1170948e30164d4f54098c1294047981b1ba
Anshul Goyal <anshulgoyal589@gmail.com>
2024-09-27 23:41:22 +0530

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const isAuthPage = pathname.startsWith('/login') || pathname.startsWith('/register');
-  const isPublicAsset = pathname.startsWith('/_next/') || pathname === '/favicon.ico' || pathname ===  '/toolsnitchlogo.png'  || pathname ===  '/carousel1.jpg'  || pathname ===  '/carousel2.jpg'  || pathname ===  '/t.jpg' ;
+  const isPublicAsset = pathname.startsWith('/_next/') || pathname === '/favicon.ico' || pathname ===  '/toolsnitchlogo.png'  || pathname ===  '/_next/image?url=%2Fgroup.png&w=3840&q=75'  || pathname ===  '/_next/image?url=%2FiPhone.png&w=3840&q=75'  || pathname ===  '/_next/image?url=%2Ft.jpg&w=3840&q=75'   || pathname ===  'next/image?url=%2Fcarousel1.jpg&w=3840&q=75'  || pathname ===  '/_next/image?url=%2Fcarousel2.jpg&w=3840&q=75';
   
   const jwtToken = request.cookies.get('jwtToken')?.value;
 
