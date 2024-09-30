dde4f1591cc978f825f7fae398229ed92bb3cf16
AnshulGoyal589 <anshulgoyal589.com>
2024-09-30 11:30:03 +0530

    deployed backend url

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://tools-snitch.yellowbush-cadc3844.centralindia.azurecontainerapps.io',
+  // baseURL: 'https://tools-snitch.yellowbush-cadc3844.centralindia.azurecontainerapps.io',
+  baseURL: 'http://3.108.98.71:8000',
   // baseURL: 'http://localhost:8000',
     headers: {
         'Content-Type': 'application/json',
