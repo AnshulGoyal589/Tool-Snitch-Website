67803fe0a0366ca5497b43d1d1f79faa45b52ab0
AnshulGoyal589 <anshulgoyal589.com>
2024-10-01 21:14:30 +0530

    shopkeepr chnages

--- src/app/(device-form)/shops/[slug]/page.tsx
+++ src/app/(device-form)/shops/[slug]/page.tsx
@@ -35,7 +35,7 @@ export default async function ShopDetailsPage({params} : {params: {slug: string}
             services: [data.devicesDeal],
             description: data.desc,
             images: data.images.map((image:string, index:number) => ({id: index, url: image, name: `image-${index}`})),
-            city: data.shopKeeper.state
+            // city: data.shopKeeper.state
         }
         return (
             <>
