3d7e2c5f2b9d0a9e0e388e646cf4c300918bc3a2
AnshulGoyal589 <anshulgoyal589.com>
2024-10-01 20:37:52 +0530

    backend url chnages

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const api = axios.create({
   // baseURL: 'https://tools-snitch.yellowbush-cadc3844.centralindia.azurecontainerapps.io',
-  baseURL: 'https://toolsnitch.work.gd',
+  baseURL: 'https://toolsnitch.ninja',
   // baseURL: 'http://localhost:8000',
     headers: {
         'Content-Type': 'application/json',
